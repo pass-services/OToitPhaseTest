de244bd9ace83f811ade9f66fb8f143ab544bbe1
pass-services <35070791+pass-services@users.noreply.github.com>
2018-01-08 10:32:42 +0100

    Adapt translation to O'Toit context
    Also correct wrong translations

--- catalog/language/fr-Fr/product/product.php
+++ catalog/language/fr-Fr/product/product.php
@@ -3,27 +3,27 @@
 // Text
 
 $_['text_search']              = 'Chercher';
-$_['text_brand']               = 'Marque';
-$_['text_manufacturer']        = 'Marque:';
+$_['text_brand']               = 'Producteur';
+$_['text_manufacturer']        = 'Producteur:';
 $_['text_model']               = 'Code produit:';
-$_['text_reward']              = 'Points de récompense:';
-$_['text_points']              = 'Prix ​​en points de récompense:';
+$_['text_reward']              = 'Points de fidélité:';
+$_['text_points']              = 'Prix ​​en points de fidélité:';
 $_['text_stock']               = 'Disponibilité:';
 $_['text_instock']             = 'En stock';
 $_['text_tax']                 = 'Taxe Ex:';
 $_['text_discount']            = 'ou plus';
 $_['text_option']              = 'Options disponibles';
-$_['text_minimum']             = 'Ce produit a une le minimum quantité de %s';
+$_['text_minimum']             = 'Ce produit a une quantité minimum de commande de %s';
 $_['text_reviews']             = '%s avis';
-$_['text_write']               = 'Écrire une critique';
+$_['text_write']               = 'Écrire un avis';
 $_['text_login']               = 'S`il vous plaît <a href="%s"> s`identifier </a> ou <a href="%s"> registre </a> réviser';
-$_['text_no_reviews']          = 'Il n`y a aucune critique sur ce produit.';
+$_['text_no_reviews']          = 'Il n`y a aucun avis sur ce produit.';
 $_['text_note']                = ' <span class="text-danger"> Remarque: </span> Le HTML n`est pas traduit!';
-$_['text_success']             = 'Merci pour votre avis. Il a été soumis au webmaster pour approbation.';
+$_['text_success']             = 'Merci pour votre avis. Il a été soumis au responsable pour approbation.';
 $_['text_related']             = 'Produits connexes';
 $_['text_tags']                = 'Mots clés:';
 $_['text_error']               = 'Produit non trouvé!';
-$_['text_payment_recurring']   = 'Profil de paiement';
+$_['text_payment_recurring']   = 'Paiement récurrent';
 $_['text_trial_description']   = '%s chaque %d %s( s ) pour %d Paiement ( s ) puis';
 $_['text_payment_description'] = '%s chaque %d %s( s ) pour %d Paiement ( s )';
 $_['text_payment_cancel']      = '%s chaque %d %s( s ) jusqu`à annulé';
@@ -40,8 +40,8 @@ $_['entry_qty']                = 'Qté';
 $_['entry_name']               = 'votre nom';
 $_['entry_review']             = 'Votre avis';
 $_['entry_rating']             = 'Évaluation';
-$_['entry_good']               = 'Bien';
-$_['entry_bad']                = 'Mal';
+$_['entry_good']               = 'Bon';
+$_['entry_bad']                = 'Mauvais';
 
 
 // Tabs
@@ -53,8 +53,8 @@ $_['tab_review']               = 'Avis (%s)';
 
 // Error
 
-$_['error_name']               = 'Attention: Le nom de la revue doit comporter entre 3 et 25 caractères!';
-$_['error_text']               = 'Attention: Le texte de révision doit comporter entre 25 et 1000 caractères!';
+$_['error_name']               = 'Attention: Le nom de l`avis doit comporter entre 3 et 25 caractères!';
+$_['error_text']               = 'Attention: Le texte de l`avis doit comporter entre 25 et 1000 caractères!';
 $_['error_rating']             = 'Attention: Veuillez sélectionner une évaluation!';
 
 
