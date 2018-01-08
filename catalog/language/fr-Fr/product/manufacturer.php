87b8f80f240cc6f775d7a3b8551d7610f0630220
pass-services <35070791+pass-services@users.noreply.github.com>
2018-01-08 10:29:27 +0100

    Adapt translation to O'Toit context

--- catalog/language/fr-Fr/product/manufacturer.php
+++ catalog/language/fr-Fr/product/manufacturer.php
@@ -7,12 +7,12 @@ $_['heading_title']     = 'Trouvez votre marque préférée';
 
 // Text
 
-$_['text_brand']        = 'Marque';
+$_['text_brand']        = 'Producteur';
 $_['text_index']        = 'Indice de marque:';
 $_['text_error']        = 'Marque non trouvée!';
 $_['text_empty']        = 'Il n`y a aucun produit à lister.';
 $_['text_quantity']     = 'Qté:';
-$_['text_manufacturer'] = 'Marque:';
+$_['text_manufacturer'] = 'Producteur:';
 $_['text_model']        = 'Code produit:';
 $_['text_points']       = 'Points de récompense:';
 $_['text_price']        = 'Prix:';
@@ -24,8 +24,8 @@ $_['text_name_asc']     = 'prénom ( UNE - Z )';
 $_['text_name_desc']    = 'prénom ( Z - UNE )';
 $_['text_price_asc']    = 'Prix ( Faible &gt; Haute )';
 $_['text_price_desc']   = 'Prix ( Haute &gt; Faible )';
-$_['text_rating_asc']   = 'Évaluation ( Le plus bas )';
-$_['text_rating_desc']  = 'Évaluation ( Le plus élevé )';
+$_['text_rating_asc']   = 'Évaluation ( La plus basse )';
+$_['text_rating_desc']  = 'Évaluation ( La plus élevée )';
 $_['text_model_asc']    = 'maquette ( UNE - Z )';
 $_['text_model_desc']   = 'maquette ( Z - UNE )';
 $_['text_limit']        = 'Montrer:';
