b7fef0f0903f06c35618700bfe5140150df208be
pass-services <35070791+pass-services@users.noreply.github.com>
2018-01-24 15:53:14 +0100

    E-Shop only works with HTTPS

--- mon-toit/admin/config.php
+++ mon-toit/admin/config.php
@@ -1,7 +1,7 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://o-toit.fr/admin/');
-define('HTTP_CATALOG', 'http://o-toit.fr/');
+define('HTTP_SERVER', 'https://o-toit.fr/admin/');
+define('HTTP_CATALOG', 'https://o-toit.fr/');
 
 // HTTPS
 define('HTTPS_SERVER', 'https://o-toit.fr/admin/');
